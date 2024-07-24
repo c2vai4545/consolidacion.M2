b6a0a0ed32c4074ba0fd4ed4ea32e6e36e9e3671
Carlos Núñez <c2vai2003@me.com>
2024-07-24 17:56:24 -0300

    Update scripts.js
    Se documenta funcionalidad de JS

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,5 @@
+/* Este codigo JavaScript agrega un listener al elemento form con el id 'contactForm'. Cuando el form 
+sea enviado, la fucion provista como segundo argumento sera ejecutada. */
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('¡Formulario enviado con éxito!');
